185441f1108362b4b71c6dec4b82d08ac72883db
James Lamb <jaylamb20@gmail.com>
2022-11-28 21:00:06 -0600

    more changes

--- setup.sh
+++ setup.sh
@@ -8,36 +8,36 @@ sudo apt-get install -y \
     gnupg \
     lsb-release
 
-# curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
-# | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg \
+| sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-# echo \
-#   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-#   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-# sudo apt-get update -y
-# sudo apt-get install -y \
-#     docker-ce \
-#     docker-ce-cli \
-#     containerd.io
+sudo apt-get update -y
+sudo apt-get install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io
 
-# sudo systemctl enable docker
-# sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl start docker
 
-# # install nvidia-docker
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-#       && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-#       && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
-#             sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-#             sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+# install nvidia-docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-#     && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add - \
-#     && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/gpgkey | sudo apt-key add - \
+    && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-# distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
-#     && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
-#     && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+    && curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add - \
+    && curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 # get nvidia drivers and tools
 # (versions selected by choosing the newest thing shown in `apt search nvidia-driver`)
